94f2fd62812d6e5a41cba078ad5922a55f3784f6
Amol Yadav <amolyadav6125@gmail.com>
2025-12-30 13:17:36 +0530

    dockerified

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173", "https://cosinv.com"],
     methods: ["GET", "POST"]
   }
 });
